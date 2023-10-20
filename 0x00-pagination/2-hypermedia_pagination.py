7f1c6799350ad94710345f183ae92924b0eacd0b
Tony <nya.tony2010@gmail.com>
2023-10-20 18:39:07 +0100

    Update: '2-hypermedia_pagination.py' file

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -28,16 +28,15 @@ def index_range(page: int, page_size: int) -> Tuple[int]:
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -47,8 +46,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Get the range of the dataset
-        """
+        """Get the range of the dataset"""
         # Use assert to verify if both arguments are integers greater than 0
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
@@ -67,8 +65,7 @@ class Server:
         return dataset[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
-        """Get info of some pages
-        """
+        """Get info of some pages"""
         # Set an empty dict
         pages = {}
 
@@ -86,13 +83,15 @@ class Server:
         total_pages = round(n / size) if size > 0 else ceil(n / page_size)
 
         # Update the dict
-        pages.update({
-            'page_size': size,
-            'page': page,
-            'data': data,
-            'next_page': next_page,
-            'prev_page': prev_page,
-            'total_pages': total_pages
-        })
+        pages.update(
+            {
+                "page_size": size,
+                "page": page,
+                "data": data,
+                "next_page": next_page,
+                "prev_page": prev_page,
+                "total_pages": total_pages,
+            }
+        )
 
         return pages
