3fa163b2108cbf5a7a51368d3c9243e602ca263e
Tony <nya.tony2010@gmail.com>
2023-10-20 16:50:37 +0100

    Add: '1-simple_pagination.py' file

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""
+Simple pagination
+"""
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    """
+    Definition of the function index_range
+
+    The function should return a tuple of size two containing
+    a start index and an end index corresponding to the range of indexes
+    to return in a list for those particular pagination parameters.
+
+    Page numbers are 1-indexed, i.e. the first page is page 1.
+
+    :param page: integer
+    :param page_size: integer
+    :return: a tuple of start index and end index
+    """
+    end = page * page_size
+    start = end - page_size
+
+    return start, end
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get the range of the dataset
+        """
+        # Use assert to verify if both arguments are integers greater than 0
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+
+        # Get the start and end indexes to paginate the dataset
+        start, end = index_range(page, page_size)
+
+        # Get the dataset
+        dataset = self.dataset()
+        n = len(dataset)
+
+        # Check if pages are out of range
+        if start > n:
+            return []
+
+        return dataset[start:end]
+
+
+        # if start > n:
+        #     return []
+        #
+        # return dataset[start, end]
