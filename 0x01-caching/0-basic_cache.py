d87e2d2fbf73a4513a667cf6e029fdb34c6dd3ee
Tony <nya.tony2010@gmail.com>
2023-10-25 08:08:31 +0100

    Add: '0-basic_cache.py' file

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Basic dictionary
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache inherist from BaseCaching
+      - implements a 'put' method
+      - implements a 'get' method
+    """
+    def __init__(self):
+        """ Initialize this child class using the parent class """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key is None or item is None:
+            return
+        
+        self.cache_data.update({
+            key: item
+        })
+
+    def get(self, key):
+        """ Get the value from the cache linked to key """
+        self.cache_data.get(key, None)
