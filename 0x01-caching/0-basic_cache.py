36ec76a9e728912cae9e0d25d361b4eea408783c
Tony <nya.tony2010@gmail.com>
2023-10-25 08:10:48 +0100

    Update: '0-basic_cache.py' file

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,23 +5,22 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ BasicCache inherist from BaseCaching
-      - implements a 'put' method
-      - implements a 'get' method
+    """BasicCache inherist from BaseCaching
+    - implements a 'put' method
+    - implements a 'get' method
     """
+
     def __init__(self):
-        """ Initialize this child class using the parent class """
+        """Initialize this child class using the parent class"""
         super().__init__()
 
     def put(self, key, item):
-        """ Add an item to the cache """
+        """Add an item to the cache"""
         if key is None or item is None:
             return
-        
-        self.cache_data.update({
-            key: item
-        })
+
+        self.cache_data.update({key: item})
 
     def get(self, key):
-        """ Get the value from the cache linked to key """
+        """Get the value from the cache linked to key"""
         self.cache_data.get(key, None)
